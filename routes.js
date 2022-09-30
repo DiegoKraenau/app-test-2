8e7135f0ca9fb58d8cc408d3945d9bbedb6ddee6
diegokraenau <diegokraenau@gmail.com>
2022-09-30 17:07:25 -0500

    Fix

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
+const appVersion = require('./package.json').version;
+
 module.exports = (app) => {
   app.get("/", (req, res) => {
-    res.send("Hello from app-test-2!");
+    res.send(`Hello from app-test-1! and running version ${appVersion}`);
   });
 
   app.get("/test", (req, res) => {
-    res.send("Testing routes app-test-2");
+    res.send(`Testing routes app-test-1! and running version ${appVersion}`);
   });
 
   return app;
